791bdc2a8c93172b530a32793f47043523bf6aeb
Chresten Søndergaad <crsondergaard@gmail.com>
2026-08-08 11:45:17 +0200

    Add text processing functions for tokenization and decoding in util.py

--- src/sturnus/util.py
+++ src/sturnus/util.py
@@ -79,3 +79,13 @@ def generate(
         idx = torch.cat((idx, idx_next), dim=1)
 
     return idx
+
+def text_to_tokens(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    return encoded_tensor
+
+def tokens_to_text(tokens, tokenizer):
+    return tokenizer.decode(tokens.squeeze(0).tolist())
+
+
